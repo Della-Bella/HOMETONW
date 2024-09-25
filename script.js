d47d2c905c069255e213dd65865f1dd9237c8ff7
Della Bella <48986120+Della-Bella@users.noreply.github.com>
2024-09-25 20:32:52 +0100

    excercise 5
    Excercise 5

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ let photos, carousel;
 photos = ['img/1_beach.jpg', 'img/2_beach.jpg', 'img/3_beach.jpg'];
 carousel = ['img/3_beach.jpg'];
 
-// Assuming you have an <img> element with id="pic"
 let element_pic = document.getElementById('pic');
 
-// Set the src attribute of the img element to display the first photo from the array
 element_pic.src = photos[0];
 document.getElementById('next').addEventListener('click', (event) => {
     let element_pic2 = document.getElementById('pic');
